7265f621ea80784a281779d770834ac0fa5475af
john <johnmontes1995@gmail.com>
2019-01-07 16:05:18 +0000

    new changes

--- inmunizationSheet.php
+++ inmunizationSheet.php
@@ -58,7 +58,7 @@ if(isset($_GET['download'])) {
     $name = $_GET['name'];
     $pdf = new PDF('P','mm','A4');
     $data= $pdf->LoadData("datos.txt");
-    $pdf->SetFont('Arial','',7);
+    $pdf->SetFont('Arial','',9);
     $pdf->SetLeftMargin(25);
     $pdf->SetRightMargin(25);
 
@@ -70,17 +70,59 @@ if(isset($_GET['download'])) {
     $text .= "RESPONSIBLE INVESTIGATOR(S): " . "\n";
     $text .= "SPONSOR: " . "\n";
     $text .= "DATE: ";
-    $pdf->MultiCell(0, 6, $text, 1, 'L');
+    $pdf->MultiCell(0, 6.2, $text, 1, 'L');
     $pdf->Line(25, 80, 180, 80);
     $pdf->Ln(20);
     $pdf->SetFont('Arial','BU',12);
     $pdf->Cell(15, 7, 'ANTIGEN PREPARATION');
 
+    $text1 = "ANTIGEN: " . "\n";
+    $text1 .= " - Supplied by: " .  "hohohohohohohohhohohohohohohohohho" . "\n";
+    $text1 .= " - Labeled: " . "\n";
+    $text1 .= " - Date delivered: " . "\n";
+    $text1 .= " - Format: " . "\n";
+    $text1 .= " 1) " . "\n";
+    $text1 .= " 2) Vol: " . "\n";
+    $text1 .= " 3) Total vol. needed: " . "\n";
+    $text1 .= " 4) Aliquots: " . "\n";
+    $text1 .= " 5) Conc: " . "\n";
+    $text1 .= " 6) Inject: " . "\n";
+    $text1 .= "\n" . "\n" . "\n" ."\n" .
+        "\n" . "\n" . "\n" . "\n" . "\n" . "\n" . "\n" . "\n" . "\n";
+
+    $pdf->Ln(11);
+    $pdf->SetFont('Arial','',9);
+    $pdf->MultiCell(0, 6, $text1, 1, 'L');
+
+
+
     //Second page
     $pdf->AddPage();
+    $pdf->SetFont('Arial','U',11);
+    $pdf->Cell(15, 7, 'ANTIGEN EMULSION PREPARATION');
+    $pdf->Ln(11);
+    $pdf->SetFont('Arial','',9);
+    $pdf->MultiCell(0, 200, '', 1, 'L');
+
 
     //Third page
     $pdf->AddPage();
+    $pdf->Ln(8);
+    $pdf->SetFont('Arial','BU',11);
+    $pdf->Cell(15, 7, 'IMMUNIZATION PROCEDURE');
+    $pdf->Ln(9);
+    $pdf->SetFont('Arial','',10.5);
+    $text2 = "Birds allocated (Birds ID, relevant notes on experimental" .
+        " animals such as health, behaviour and welfare issues)";
+    $pdf->MultiCell(0, 6, $text2);
+    $pdf->Ln(5);
+
+    $pdf->Cell(15, 5, "Immunization timeline");
+    $pdf->Ln(5);
+
+    $titles = array('INJ.', 'DAY', 'DATE', 'VOLUME(ul)', 'AMOUNT(ug)', 'TYPE', 'NOTES');
+
+    
     //Download name
     $pdf->Output('I', $name . '.pdf');
 }
