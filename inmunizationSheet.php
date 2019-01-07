02eaea30d5013744d6d9e17302afd8fd6df3e023
John Alexander Montes López <johnmontes1995@gmail.com>
2019-01-07 17:19:11 +0000

    inmunization sheet finished

--- inmunizationSheet.php
+++ inmunizationSheet.php
@@ -38,19 +38,112 @@ class PDF extends FPDF
 
     function BasicTable($headers, $data)
     {
-        // Cabecera
-        foreach ($headers as $header) {
-            $this->Cell(17, 7, $header, 1, 0, 'C');
-        }
-        $this->Ln();
-        // Datos
 
-        foreach($data as $row)
-        {
-            foreach($row as $col)
-                $this->Cell(17,7,$col,1);
-            $this->Ln();
-        }
+        //Table
+        $x = $this->GetX();
+        $y = $this->GetY();
+        $text = "BIRD # " . "\n" . $data;
+        $this->MultiCell(22, 12.5, $text, 1, 'C');
+        $this->SetXY($x+22, $y);
+        //First row
+        $this->Cell(14, 5, $headers['0'], 1, 0, 'C');
+        $this->Cell(14, 5, $headers['1'], 1, 0, 'C');
+        $this->Cell(23, 5, $headers['2'], 1, 0, 'C');
+        $this->Cell(24, 5, $headers['3'], 1, 0, 'C');
+        $this->Cell(24, 5, $headers['4'], 1, 0, 'C');
+        $this->Cell(19, 5, $headers['5'], 1, 0, 'C');
+        $this->Cell(19, 5, $headers['6'], 1, 0, 'C');
+        // Second row
+        $this->SetXY($x+22, $y+5);
+        $this->Cell(14, 5, '1', 1, 0, 'C');
+        $this->Cell(14, 5, 'D0', 1, 0, 'C');
+        $this->Cell(23, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+
+        //Third row
+        $this->SetXY($x+22, $y+10);
+        $this->Cell(14, 5, '2', 1, 0, 'C');
+        $this->Cell(14, 5, 'D14', 1, 0, 'C');
+        $this->Cell(23, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+
+        //Fourth row
+        $this->SetXY($x+22, $y+15);
+        $this->Cell(14, 5, '3', 1, 0, 'C');
+        $this->Cell(14, 5, 'D28', 1, 0, 'C');
+        $this->Cell(23, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+
+        //Fifth row
+        $this->SetXY($x+22, $y+20);
+        $this->Cell(14, 5, '4', 1, 0, 'C');
+        $this->Cell(14, 5, 'D42', 1, 0, 'C');
+        $this->Cell(23, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(24, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+        $this->Cell(19, 5, '', 1, 0, 'C');
+
+        $this->Write(5, "IM - intramuscular");
+
+        $this->Ln(10);
+
+    }
+
+    function lastTable($headers){
+
+        //First row
+
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(22, 5, 'BIRD', 1, 0, 'C');
+        $this->Cell(22, 5, 'D0', 1, 0, 'C');
+        $this->Cell(22, 5, 'D14', 1, 0, 'C');
+        $this->Cell(22, 5, 'D28', 1, 0, 'C');
+        $this->Cell(22, 5, 'D42', 1, 0, 'C');
+        $this->Cell(22, 5, '', 1, 0, 'C');
+        $this->Cell(22, 5, '', 1, 0, 'C');
+        $this->Ln();
+
+
+        $this->SetFont('Arial', '', 10);
+        //Second row
+
+        $this->Cell(22, 10, $headers['0'], 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Ln();
+
+        //Third row
+        $this->Cell(22, 10, $headers['1'], 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Ln();
+
+        //Fourth row
+        $this->Cell(22, 10, $headers['2'], 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
+        $this->Cell(22, 10, '', 1, 0, 'C');
     }
 }
 
@@ -116,13 +209,25 @@ if(isset($_GET['download'])) {
         " animals such as health, behaviour and welfare issues)";
     $pdf->MultiCell(0, 6, $text2);
     $pdf->Ln(5);
-
     $pdf->Cell(15, 5, "Immunization timeline");
-    $pdf->Ln(5);
-
+    $pdf->Ln(8);
     $titles = array('INJ.', 'DAY', 'DATE', 'VOLUME(ul)', 'AMOUNT(ug)', 'TYPE', 'NOTES');
+    $birdcolor = array('RED', 'GREEN', 'BLUE');
+    //First table
+    $pdf->BasicTable($titles,  $birdcolor['0']);
 
-    
+
+    //Second table
+    $pdf->BasicTable($titles, $birdcolor['1']);
+
+
+    //Third table
+    $pdf->BasicTable($titles,  $birdcolor['2']);
+
+
+    //Fourth table
+    $pdf->lastTable($birdcolor);
+
     //Download name
     $pdf->Output('I', $name . '.pdf');
 }
